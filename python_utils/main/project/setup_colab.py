48926aba156d8c878ada24c81183d62a17a234dd
sebascal1 <60772784+sebascal1@users.noreply.github.com>
2020-12-15 01:07:22 -0500

    Update setup_colab.py

--- python_utils/main/project/setup_colab.py
+++ python_utils/main/project/setup_colab.py
@@ -14,10 +14,22 @@ def setup_software():
     vis_path = "project/visualization_utils.py"
     layers_path = "project/layers.py"
     train_path = "project/train_utils.py"
+    lung_path = "project/lung_segment.py"
     setup_general.download_github_content(torch_path, "utils/torch_utils.py")
     setup_general.download_github_content(layers_path, "utils/layers.py")
     setup_general.download_github_content(train_path, "utils/train_utils.py")
     setup_general.download_github_content(vis_path, "utils/visualization_utils.py")
+    setup_general.download_github_content(vis_path, "utils/lung_segment.py")
+    
+    from utils import general as gen
+    if download_dataset:
+        train_id ="1zaucizp_3iy_Tlk4NNfNqEtP25qcSKLl"
+        #test_id = 
+        gen.download_file_from_google_drive(train_id, "train_data.zip", size=1.96e6)
+        #gen.download_file_from_google_drive(test_id, "test_data.zip", size=805.2e3)
+        print("Dataset Downloaded Successfully")
+    
+    
     print("Util Functions Downloaded Successfully")
     print("Software Demo Enabled Successfully")
    
