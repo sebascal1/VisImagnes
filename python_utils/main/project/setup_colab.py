445f758e0801e347b12bad2b7f3ff8ac44845916
sebascal1 <60772784+sebascal1@users.noreply.github.com>
2020-12-16 13:59:55 -0500

    Update setup_colab.py

--- python_utils/main/project/setup_colab.py
+++ python_utils/main/project/setup_colab.py
@@ -51,9 +51,9 @@ def setup_journal():
     train_set_thresh ="1yN2dzVPjz-5yTMZfeTrMRTDQVCgkmwaU"
     train_set_segment = "1Ar7ww1ZNjsYs9SpQWmXdVDeDbbdIzV6H"
     test_set_original = "QLuziDEys8G9tp5d-6-X9hnlusy5MwgK/view?usp=sharing"
-    gen.download_file_from_google_drive(train_set_1, "data_tresh.zip", size=216e3)
-    gen.download_file_from_google_drive(train_set_2, "data_segment.zip", size=55.7e3)
-    gen.download_file_from_google_drive(train_set_2, "data_original.zip", size=549.7e3)
+    gen.download_file_from_google_drive(train_set_thresh, "data_tresh.zip", size=216e3)
+    gen.download_file_from_google_drive(train_set_segment, "data_segment.zip", size=55.7e3)
+    gen.download_file_from_google_drive(test_set_original, "data_original.zip", size=549.7e3)
     
     originalData_id = "1CXyrOF1KEeZUWtKNM3GWaI3Evqwg48-n"
     threhsoldData_id = "1l4BlO2APgdMNyuIyQ0dm3sDIB9zgASzR"
